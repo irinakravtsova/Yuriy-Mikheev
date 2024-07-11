eb9bb51075803bc56a4564a76a3f5a8c08aeec2a
Irina Kravtsova <kosmonavt980@gmail.com>
2024-07-11 08:00:20 +0300

    corr. hero-text

--- src/components/main/InfoBox/InfoBox.jsx
+++ src/components/main/InfoBox/InfoBox.jsx
@@ -5,25 +5,29 @@ import Heading from '../../Heading/Heading';
 import Subtitle from '../Subtitle/Subtitle';
 import TextBlock from '../TextBloсk/TextBloсk';
 import Button from '../../button/button';
+import LinkButton from '../../linkButton/linkButton';
 
 
 function InfoBox() {
   return (
     <div className='hero__info'>
+      <div className='hero__title-box'>
+        < Heading
+          level = 'h1'
+          isClass = {'hero-title'}
+          text = 'Я помогаю вам '
+        />
+        < Subtitle
+          isClass = {'hero-subtitle'}
+          text = 'повысить качество вашей жизни'
+        />
+      </div>
     
-      < Heading
-        level = 'h1'
-        isClass = {'hero-title'}
-        text = 'Я помогаю вам '
-      />
-      < Subtitle
-        isClass = {'hero-subtitle'}
-        text = 'повысить качество вашей жизни'
-      />
+    
       < TextBlock
         isClass = {'text'}
          /> 
-      < Button 
+      < LinkButton 
         isClass = {'hero__info-button'}
         text = 'Начать'      
       />
